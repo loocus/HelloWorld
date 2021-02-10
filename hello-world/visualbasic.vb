92f5679e475ee1f4740c447ff4cd2084d43a0661
Orioniis <69150061+Orioniis@users.noreply.github.com>
2021-02-10 22:55:15 +0100

    Update visualbasic.vb

--- hello-world/visualbasic.vb
+++ hello-world/visualbasic.vb
@@ -4,5 +4,6 @@ Module Main
     Sub Main()
         Console.WriteLine("Utilité de ce langage ? 0.")
         Console.WriteLine("Pour le moment =)")
+        Console.WritLine("Btw le mec qui a écrit ça ne ce rend pas compte du potentiel de Visual Basic mais bon trql c'est un singe x)")
     End Sub 
 End Module
