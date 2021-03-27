4737d8b36dccdfae9d36df5fe8ad0e7343e35516
Galzronn <69150061+Galzronn@users.noreply.github.com>
2021-03-28 00:02:47 +0100

    Update visualbasic.vb

--- hello-world/visualbasic.vb
+++ hello-world/visualbasic.vb
@@ -6,6 +6,6 @@ Module Main
         Console.WriteLine("Pour le moment =)")
         Console.WriteLine("Btw le mec qui a écrit ça ne ce rend pas compte du potentiel de Visual Basic mais bon trql c'est un singe x)")
         Console.WriteLine("je vous invite a ne pas aller voir son github : github.com/Akuramu mais allez plutot voir le mien")
-        Console.WritLine("github.com/Orioniis")
+        Console.WritLine("github.com/Galzronn")
     End Sub 
 End Module
